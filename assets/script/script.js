93f19ff7e7a9ab6948a4192cf2031490f8de8c94
Alex Gerraty <alexandergerraty@gmail.com>
2023-10-09 19:48:17 +1100

    firs page updated

--- assets/script/script.js
+++ assets/script/script.js
@@ -62,3 +62,11 @@ getMondegreen(songName, artistName);
 
 
 
+
+  var button = document.getElementById("start");
+  var input= document.getElementById("input1");
+
+    button.addEventListener("click",(e) => {
+     input.style.display ="flex";
+ 
+  })

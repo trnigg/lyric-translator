07b53ad6fd7d7158137c15441834565203b59c69
Mohamed Amin A Osman <mo5250760@gmail.com>
2023-10-11 20:27:08 +1100

    fixed the input Bug

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,20 +1,19 @@
 
-var startButton = document.getElementById("start");
+var button = document.getElementById("start");
 var input1= document.getElementById("input1");
-let inputArtist = document.querySelector("#input-artist").value;
-let inputSong = document.querySelector("#input-song").value;
-
-// when start button is clicked it displays the two inputs.
-startButton.addEventListener("click",(e) => {
+  button.addEventListener("click",(e) => {
    input1.style.display ="flex";
 });
 
-// Saves the input value to the local storage when the songdefy button is clicked.
+
 document.querySelector("#songdefy-btn").addEventListener("click", function() {
-window.inputValue = inputSong;
+// stores song input value into the local storage.
+let inputSong = document.querySelector("#input-song").value;
+window.inputValue = inputSong; 
 localStorage.setItem('inputSong', inputSong); 
+// stores artist input value into the local storage.
+let inputArtist = document.querySelector("#input-artist").value;
 window.inputValue = inputArtist; 
 localStorage.setItem('inputArtist', inputArtist); 
-
 
 })

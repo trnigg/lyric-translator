62c601987e50d0c229ee8edd50d097aa20dccf8b
Alex Gerraty <alexandergerraty@gmail.com>
2023-10-12 14:08:57 +1100

    modal

--- assets/script/index.js
+++ assets/script/index.js
@@ -29,31 +29,36 @@ localStorage.setItem('inputArtist', inputArtist); // Using localStorage
 
 
 
-// Get the modal
+// Get all the elements
 var modal = document.getElementById("songModal");
 
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close-modal")[0];
 
-// When the user clicks the button, open the modal 
-button.onclick = function openModal() {
-  modal.style.display = "block";
-}
+// When the user clicks the button, the modal is opened or if the both boxes are filled move to Second Page 
+document.getElementById("songdefy-btn").addEventListener("click", function () {
+ var inputSong = document.getElementById("input-song").value;
+var inputArtist = document.getElementById("input-artist").value;
+  if (inputSong === "" || inputArtist === "") {
+      modal.style.display = "block";
+  } else {
+      // Redirect to Second page
+      window.location.href = "SecondPage.html";
+  }
+});
 
-// When the user clicks on <span> (x), close the modal
+
+// When the user clicks on (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close the modal
 window.onclick = function close2Modal(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-var btnDefy = document.getElementById("btnDefy");
-var songInput = document.getElementsByClassName("input-song");
-var artistInput = document.getElementsByClassName("input-artist");
 
